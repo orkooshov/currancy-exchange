7a200b0fc19b79e3627afcc04ac0a9360eea5fd4
Mukhammed <orkooshov@gmail.com>
2021-02-28 02:52:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then(response => response.json())
+    .then(data => {
+        loadDate(new Date(data.Date));
+        let text = makeHtmlText(data);
+        loadToTable(text);
+    });
+
+function loadDate(date) {
+    let dateDiv = document.getElementById('date');
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+    };
+    dateDiv.innerHTML = date.toLocaleDateString('ru', options);
+}
+
+function makeHtmlText(data) {
+    let text = '';
+    for (let elem in data.Valute) {
+        let attr = '';
+        switch (data.Valute[elem].CharCode) {
+            case 'KZT':
+                attr = `class="table-info"`;
+                break;
+            case 'USD':
+                attr = `class="table-primary"`;
+                break;
+            case 'KGS':
+                attr = `class="table-danger"`;
+                break;
+            case 'UAH':
+                attr = `class="table-warning"`;
+                break;
+            case 'UZS':
+                attr = `class="table-primary"`;
+                break;
+            default:
+                break;
+        }
+        text +=
+            `<tr ${attr}>
+                    <td>${data.Valute[elem].CharCode} (${data.Valute[elem].NumCode})</td>
+                    <td>${data.Valute[elem].Name}</td>
+                    <td>${data.Valute[elem].Value}</td>
+                </tr>`;
+    }
+    return text;
+}
+
+function loadToTable(htmlText) {
+    let table = document.getElementById('currencyRate');
+    table.innerHTML += htmlText;
+}
